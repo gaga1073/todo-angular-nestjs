ab104cc6992eaa4cf3323c97c037ae6cfe96f7e4
gaga1073 <abnormalize414@outlook.jp>
2025-10-10 08:19:30 +0900

    グループクエリサービス追加

---
+++ todo-nestjs-10/src/features/user-management/services/group-query.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { PrismaProvider } from '@/core/providers/prisma.provider';
+import { GetGroupResponse } from '@/user-management/dto/response/get-group.response';
+
+@Injectable()
+export class GroupQueryService {
+  constructor(private readonly prisma: PrismaProvider) {}
+
+  public async getGroup(groupId: string): Promise<GetGroupResponse> {
+    const row = await this.prisma.groupModel.findUniqueOrThrow({
+      where: {
+        id: groupId,
+      },
+      include: {
+        users: true,
+      },
+    });
+
+    return plainToInstance(GetGroupResponse, row);
+  }
+}
+
+// {
+//   id: '01K4QB25X33AHEGX765NRDCEC9',
+//   name: 'group1',
+//   description: '説明テスト',
+//   createAt: 2025-09-09T03:00:00.000Z,
+//   updateAt: 2025-09-09T03:00:00.000Z,
+//   users: [
+//     {
+//       userId: '01HZYC2028WMB3NJ16WCV9Z9E0',
+//       groupId: '01K4QB25X33AHEGX765NRDCEC9',
+//       role: 'owner',
+//       createAt: 2025-09-09T03:00:00.000Z,
+//       updateAt: 2025-09-09T03:00:00.000Z
+//     },
+//     {
+//       userId: '01K4QD7FP0VK1ABEFHTK6P44BT',
+//       groupId: '01K4QB25X33AHEGX765NRDCEC9',
+//       role: 'member',
+//       createAt: 2025-09-09T03:00:00.000Z,
+//       updateAt: 2025-09-27T03:00:00.000Z
+//     }
+//   ]
+// }
